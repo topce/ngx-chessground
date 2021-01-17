5fdd1aaeab9bbe7e5a13e17906e37f2a23652a60
topce <slobodqn.filipovic@gmail.co>
2021-01-17 12:13:43 +0100

    add assets to lib remove service from public api

--- projects/ngx-chessground/src/public-api.ts
+++ projects/ngx-chessground/src/public-api.ts
@@ -1,7 +1,5 @@
 /*
  * Public API Surface of ngx-chessground
  */
-
-export * from './lib/ngx-chessground.service';
 export * from './lib/ngx-chessground.component';
 export * from './lib/ngx-chessground.module';
