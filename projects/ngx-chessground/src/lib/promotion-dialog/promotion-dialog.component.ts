0dd2f359301c6c05cb55b252eb8cf7edeac3550f
topce <topce@users.noreply.github.com>
2025-06-08 12:53:55 +0200

    fix: format import statements in promotion dialog component

--- projects/ngx-chessground/src/lib/promotion-dialog/promotion-dialog.component.ts
+++ projects/ngx-chessground/src/lib/promotion-dialog/promotion-dialog.component.ts
@@ -2,9 +2,9 @@ import { CommonModule } from "@angular/common";
 import { Component, Inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import {
-	MAT_DIALOG_DATA,
-	MatDialogModule,
-	type MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
 } from "@angular/material/dialog";
 import { MatIconModule } from "@angular/material/icon";
 
