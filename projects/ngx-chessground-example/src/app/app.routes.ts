ad57d9a874622be81f65c1ac2bd3af93594c6998
topce <topce.dev@gmail.com>
2025-11-21 08:48:10 +0100

    feat: introduce PGN viewer component with game navigation and replay functionality.

--- projects/ngx-chessground-example/src/app/app.routes.ts
+++ projects/ngx-chessground-example/src/app/app.routes.ts
@@ -5,5 +5,7 @@ import { HomePageComponent } from "./home-page/home-page.component";
 export const routes: Routes = [
 	{ path: "", component: HomePageComponent },
 	{ path: "chess-table", component: ChessTablePageComponent },
+	{ path: "pgn-viewer", loadComponent: () => import('./pgn-viewer/pgn-viewer.component').then(m => m.PgnViewerComponent) },
 	{ path: "**", redirectTo: "" }, // Redirect any unknown routes to home
+	// Trigger rebuild
 ];
