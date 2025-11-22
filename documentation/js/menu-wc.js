011f48708743e85ddbd4e2fe4098c1dfc866b951
topce <topce.dev@gmail.com>
2025-11-22 08:16:57 +0100

    update documentation

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -23,16 +23,18 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                 <li class="chapter">
                     <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
                     <ul class="links">
-                        <li class="link">
-                            <a href="overview.html" data-type="chapter-link">
-                                <span class="icon ion-ios-keypad"></span>Overview
-                            </a>
-                        </li>
-                        <li class="link">
-                            <a href="index.html" data-type="chapter-link">
-                                <span class="icon ion-ios-paper"></span>README
-                            </a>
-                        </li>
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
                         <li class="link">
                             <a href="license.html"  data-type="chapter-link">
                                 <span class="icon ion-ios-paper"></span>LICENSE
@@ -48,6 +50,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                         <span class="icon ion-ios-apps"></span>Properties
                                     </a>
                                 </li>
+
                     </ul>
                 </li>
                     <li class="chapter">
@@ -64,6 +67,12 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <li class="link">
                                 <a href="components/NgxChessgroundTableComponent.html" data-type="entity-link" >NgxChessgroundTableComponent</a>
                             </li>
+                            <li class="link">
+                                <a href="components/NgxPgnViewerComponent.html" data-type="entity-link" >NgxPgnViewerComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PromotionDialogComponent.html" data-type="entity-link" >PromotionDialogComponent</a>
+                            </li>
                         </ul>
                     </li>
                         <li class="chapter">
@@ -77,6 +86,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <li class="link">
                                     <a href="injectables/NgxChessgroundService.html" data-type="entity-link" >NgxChessgroundService</a>
                                 </li>
+                                <li class="link">
+                                    <a href="injectables/PromotionService.html" data-type="entity-link" >PromotionService</a>
+                                </li>
                             </ul>
                         </li>
                     <li class="chapter">
@@ -87,6 +99,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <span class="icon ion-ios-arrow-down"></span>
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/PromotionDialogData.html" data-type="entity-link" >PromotionDialogData</a>
+                            </li>
                             <li class="link">
                                 <a href="interfaces/Unit.html" data-type="entity-link" >Unit</a>
                             </li>
@@ -104,10 +119,16 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
                             </li>
                             <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
                                 <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
                             </li>
                         </ul>
                     </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
                     <li class="chapter">
                         <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
                     </li>
