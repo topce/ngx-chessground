91aceaf5b7b02fce8ca5ccf87ff0d8dcca01cf8f
topce <topce.dev@gmail.com>
2025-11-25 18:54:22 +0100

    fix: prepend baseHref to PGN URLs using PlatformLocation for correct path resolution.

--- projects/ngx-chessground/src/lib/pgn-viewer/pgn-viewer.component.ts
+++ projects/ngx-chessground/src/lib/pgn-viewer/pgn-viewer.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, PlatformLocation } from "@angular/common";
 import {
 	ChangeDetectionStrategy,
 	Component,
@@ -143,7 +143,7 @@ export class NgxPgnViewerComponent {
 	});
 
 
-	constructor() {
+	constructor(private platformLocation: PlatformLocation) {
 		if (typeof Worker !== 'undefined') {
 			this.worker = new Worker(new URL('./pgn-processor.worker', import.meta.url));
 			this.worker.onmessage = ({ data }) => this.handleWorkerMessage(data);
@@ -161,7 +161,9 @@ export class NgxPgnViewerComponent {
 
 		// Set default URL to previous month's file
 		const monthStr = month.toString().padStart(2, '0');
-		this.urlInput.set(`/lichess/broadcast/lichess_db_broadcast_${year}-${monthStr}.pgn.zst`);
+		const baseHref = this.platformLocation.getBaseHrefFromDOM();
+		const cleanBaseHref = baseHref === '/' ? '' : baseHref.replace(/\/$/, '');
+		this.urlInput.set(`${cleanBaseHref}/lichess/broadcast/lichess_db_broadcast_${year}-${monthStr}.pgn.zst`);
 
 		// Effect to load initial PGN if provided
 		effect(() => {
@@ -410,7 +412,11 @@ export class NgxPgnViewerComponent {
 
 		// Format: lichess_db_broadcast_YYYY-MM.pgn.zst
 		const monthStr = month.toString().padStart(2, '0');
-		const url = `/lichess/broadcast/lichess_db_broadcast_${year}-${monthStr}.pgn.zst`;
+		const baseHref = this.platformLocation.getBaseHrefFromDOM();
+		// Remove trailing slash from baseHref if present to avoid double slashes, 
+		// but ensure we handle the case where baseHref is just '/'
+		const cleanBaseHref = baseHref === '/' ? '' : baseHref.replace(/\/$/, '');
+		const url = `${cleanBaseHref}/lichess/broadcast/lichess_db_broadcast_${year}-${monthStr}.pgn.zst`;
 
 		this.urlInput.set(url);
 		this.loadFromUrl();
