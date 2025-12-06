a0fa6a28ce480671cb6a82334a4abc723c8fb299
topce <topce.dev@gmail.com>
2025-12-06 18:24:37 +0100

    chore: update dependencies and add service worker support
    - Updated Angular dependencies to the latest patch versions.
    - Changed the `update:deps` script to use `npm install --force`.
    - Added a new service worker configuration file (`ngsw-config.json`) for the ngx-chessground-example project.
    - Included various icon files for the web app in the public/icons directory.
    - Created a web app manifest file (`manifest.webmanifest`) for the ngx-chessground-example project.
    - Updated the index.html to include the manifest link and added a noscript message for users without JavaScript.
    - Modified main.ts to provide service worker support with a registration strategy.

--- projects/ngx-chessground-example/src/main.ts
+++ projects/ngx-chessground-example/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, provideZonelessChangeDetection } from "@angular/core";
+import { enableProdMode, provideZonelessChangeDetection, isDevMode } from "@angular/core";
 
 import { bootstrapApplication } from "@angular/platform-browser";
 import { provideAnimations } from "@angular/platform-browser/animations";
@@ -6,6 +6,7 @@ import { provideRouter } from "@angular/router";
 import { AppComponent } from "./app/app.component";
 import { routes } from "./app/app.routes";
 import { environment } from "./environments/environment";
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
 	enableProdMode();
@@ -15,6 +16,9 @@ bootstrapApplication(AppComponent, {
 	providers: [
 		provideRouter(routes),
 		provideAnimations(),
-		provideZonelessChangeDetection(),
+		provideZonelessChangeDetection(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
 	],
 }).catch((err) => console.error(err));
